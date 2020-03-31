ac0852f37f6c33c60b8f57fac3ddfa9af1fc7d91
Markus Masalin <markus.masalin@eduvantaa.fi>
2020-03-31 21:58:29 +0300

    Number guessing game ready

--- Mobile/App.js
+++ Mobile/App.js
@@ -1,21 +1,44 @@
-import React, { useState, Component } from 'react';
+import React, { useState, useReducer, Component } from 'react';
 import { StyleSheet, Text, View, Button, TextInput, Alert } from 'react-native';
 
 
 function App() {
   
   const [result, setResult] = useState(0);
-  const [integer1, setInteger1] = useState(0);
-  const [integer2, setInteger2] = useState(0);
+  const [guess, setGuess] = useState(0);
+  const [counter, setCounter] = useState(0);
+  const [text, setText] = useState('');
   
-  const doTheSum = () => {
-   
-    setResult(integer1+integer2)
+  
+  const startTheGame = () => {
+    const randomNumber = Math.floor(Math.random() * 100) + 1;
+    setResult(randomNumber)
+    setCounter(0)
+    setText('')
+    
     }
     
-  const doTheSubtrack = () => {
-    setResult(integer1-integer2)
-    }
+  const makeTheGuess = () => {
+     
+
+    if (guess < result){
+     
+      setText('Your guess ' + guess + ' is too low')
+      setCounter(counter + 1);
+    } 
+    
+    if (guess > result) {
+     
+      setText('Your guess ' + guess + ' is too high')
+      setCounter(counter + 1);
+    } 
+    if (guess === result) {
+      Alert.alert('Congratulation! You guessed the number in ' + counter + ' guesses')
+      setGuess(0)
+
+    }  
+  }
+    
 
   const styles = StyleSheet.create({
     container: {
@@ -49,27 +72,22 @@ function App() {
    <View style={styles.container}>
      <View style={styles.inputBox} > 
       <Text>
-       Result: {result}
+        {text}
+       
       </Text>
+     
       <TextInput
         style={styles.inputStyle}
         keyboardType={'numeric'}
-        onChangeText={text => setInteger1(parseInt(text))}
-        
-        value={integer1}
+        onChangeText={e => setGuess(parseInt(e))}
+        value={guess}
       />
-      <TextInput
-        style={{width: 200, borderColor: 'gray', borderWidth: 1}}
-        keyboardType={'numeric'}
-        onChangeText={text => setInteger2(parseInt(text))}
-        
-        value={integer2}
-      />
+      
         
       </View>
     <View style={styles.buttonBox}>
-      <Button title="+" onPress={doTheSum}  />
-      <Button title="-" onPress={doTheSubtrack} />
+      <Button title="Start the Game" onPress={startTheGame}  />
+      <Button title="Make Guess" onPress={makeTheGuess} />
        
     </View>
     <View style={styles.buttonBox}>
