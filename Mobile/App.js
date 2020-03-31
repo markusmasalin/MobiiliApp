2a864685a06c152533a7024d0a1615894815b082
Markus Masalin <markus.masalin@eduvantaa.fi>
2020-03-31 17:08:15 +0300

    Calculator ready

--- Mobile/App.js
+++ Mobile/App.js
@@ -2,26 +2,48 @@ import React, { useState, Component } from 'react';
 import { StyleSheet, Text, View, Button, TextInput, Alert } from 'react-native';
 
 
-
-
-export default function App() {
+function App() {
   
   const [result, setResult] = useState(0);
   const [integer1, setInteger1] = useState(0);
   const [integer2, setInteger2] = useState(0);
-
   
   const doTheSum = () => {
    
-    Alert.alert(integer1+integer2);
-    Alert.alert('hello');
+    setResult(integer1+integer2)
     }
     
   const doTheSubtrack = () => {
-    
-      Alert.alert(integer2);
+    setResult(integer1-integer2)
     }
 
+  const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: '#fff',
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+    buttonBox: {
+      flex: 1, 
+      flexDirection: 'row',
+      alignItems: 'center',
+      justifyContent: 'space-around',
+      
+    },
+    inputBox: {
+      flex: 1,
+      flexDirection: 'column',
+      alignItems: 'center',
+      justifyContent: 'center'
+    },
+     
+    inputStyle: {
+      width: 200, 
+      borderColor: 'gray', 
+      borderWidth: 1
+    }
+  });
 
   return (
    <View style={styles.container}>
@@ -30,24 +52,24 @@ export default function App() {
        Result: {result}
       </Text>
       <TextInput
-        style={{width: 200, borderColor: 'gray', borderWidth: 1}}
-        type={'number'}
-        keyboardType={'numeric'} 
-        onChangeText={num => setInteger1(num)}
+        style={styles.inputStyle}
+        keyboardType={'numeric'}
+        onChangeText={text => setInteger1(parseInt(text))}
+        
         value={integer1}
-        />
+      />
       <TextInput
-      style={{width: 200, borderColor: 'gray', borderWidth: 1}}
-      type={'number'}
-      keyboardType={'numeric'} 
-      onChangeText={num => setInteger2(num)}
-      value={integer2}
+        style={{width: 200, borderColor: 'gray', borderWidth: 1}}
+        keyboardType={'numeric'}
+        onChangeText={text => setInteger2(parseInt(text))}
+        
+        value={integer2}
       />
         
       </View>
     <View style={styles.buttonBox}>
-      <Button title="Button1" onPress={doTheSum}/>
-      <Button title="Button2" onPress={doTheSubtrack}/>
+      <Button title="+" onPress={doTheSum}  />
+      <Button title="-" onPress={doTheSubtrack} />
        
     </View>
     <View style={styles.buttonBox}>
@@ -58,23 +80,8 @@ export default function App() {
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  buttonBox: {
-    flex: 1, 
-    flexDirection: 'row',
-    alignItems: 'center',
-    justifyContent: 'space-around'
-  },
-  inputBox: {
-    flex: 1,
-    flexDirection: 'column',
-    alignItems: 'center',
-    justifyContent: 'center'
-  }
-});
+
+
+
+
+export default App;
