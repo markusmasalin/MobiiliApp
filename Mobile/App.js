3bca07d98fe462571f61ff5c5be1a34006fe882b
Markus Masalin <markus.masalin@eduvantaa.fi>
2020-04-03 01:04:20 +0300

    Task 3 Expand your calculator

--- Mobile/App.js
+++ Mobile/App.js
@@ -3,24 +3,30 @@ import { StyleSheet, Text, View, Button, TextInput, Alert, FlatList } from 'reac
 
 
 function App() {
-  
   const [data, setData] = useState([]);
-  const [text, setText] = useState('');
-  
-  
-  const itemAdded = () => {
-    setData([...data, {key: text}]);
-    setText('')
+  const [integer1, setInteger1] = useState(0);
+  const [integer2, setInteger2] = useState(0);
+
+
+  const doTheSum = () => {
+    const summa = integer1+integer2  
+    const teksti = (`${integer1} + ${integer2} = ${summa}`);
+    setData([...data, {key: teksti}]);
+    
     }
-  
+
+  const doTheSubtrack = () => {
+    const summa = integer1-integer2  
+    const teksti = (`${integer1} - ${integer2} = ${summa}`)
+    setData([...data, {key: teksti}]);
+    
+    }
 
   const listCleared = () => {
     setData([]);
-    setText('')
-
+    setResult(0);
   }
     
-
   const styles = StyleSheet.create({
     container: {
       flex: 1,
@@ -51,33 +57,42 @@ function App() {
   return (
    <View style={styles.container}>
       
-      <View style={styles.inputBox}>
-        <Text>
-          Shopping list
-        </Text>
-      </View>
      <View style={styles.inputBox}>
-     
+      <TextInput
+        style={{width: 200, borderColor: 'gray', borderWidth: 1}}
+        keyboardType={'numeric'}
+        onChangeText={num => setInteger1(parseInt(num))}
+        value={integer1}
+      />
+      <TextInput
+        style={{width: 200, borderColor: 'gray', borderWidth: 1}}
+        keyboardType={'numeric'}
+        onChangeText={num => setInteger2(parseInt(num))}
+        value={integer2}
+      />
+      
+    </View>
+    <View style={styles.inputBox}>
       <View style={styles.buttonBox}>
-        <Button title="Add" onPress={itemAdded}  />
-        <Button title="Clear" onPress={listCleared}  />
+          <Button title="+" onPress={doTheSum}  />
+          <Button title="-" onPress={doTheSubtrack} />
+          <Button title="Clear" onPress={listCleared}  />
       </View>
       
-      <TextInput
-        style={styles.inputStyle}
-        onChangeText={text => setText(text)}
-        value={text}
-      /> 
-      <FlatList 
-          data={data}
-          renderItem={({item}) => 
-            <Text>{item.key}</Text>
-          }
-        />
+    
     </View>
     
-    <View style={styles.inputStyle}>
-      
+  
+  
+    <View style={styles.inputBox}>
+        
+      <FlatList 
+        data={data}
+        renderItem={({item}) => 
+          <Text>{item.key}</Text>
+        }
+        keyExtractor={(item, index) => index.toString()}
+      />
     </View>
     
     </View>
